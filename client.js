0bcb683871fe3febc3eccfa65ba4b9425ffe5d22
rkiang <ryanlkiang@gmail.com>
2017-09-05 13:23:23 -0500

    added html and js

---
+++ client.js
@@ -0,0 +1,13 @@
+console.log('running!');
+
+
+function regular() {
+    if(true) {
+        var trueBlock = 'Hello';
+    } else {
+        var falseBlock = 'Goodbye';
+    }
+    console.log(falseBlock);
+}
+// call the function
+regular();
